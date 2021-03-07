54a318e48dc72e8c12c6e618ca2db51b4fc7fb65
Loryerba <lorenzoerba250@gmail.com>
2021-03-07 15:51:30 +0100

    Completed Log and Login

--- Lavori in corso/Personale/adminPnl.php
+++ Lavori in corso/Personale/adminPnl.php
@@ -1,26 +1,83 @@
+<?php
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+if (!isset($_SESSION['user']) || !$_SESSION['logged'] = true) {
+    //redirect alla pagina 1
+    redirect("7");
+} else if (time() - $_SESSION['timesetted'] > 10) {
+    //eliminazione della sessione timesetted
+    unset($_SESSION['timesetted']);
+    //eliminazione della sessione user
+    unset($_SESSION['user']);
+    //eliminazione della sessione logged
+    unset($_SESSION['logged']);
+    //redirect alla pagina 1
+    redirect("7");
+}
+
+
+
+function redirect($errortype)
+
+{
+
+    header("location: login.php?error=$errortype");
+
+    exit();
+}
+?>
+
 <!DOCTYPE html>
+
 <html>
+
 <head>
+
     <meta charset='utf-8'>
+
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+
     <title>Pannello Amministrativo</title>
+
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+
     <link rel='stylesheet' type='text/css' media='screen' href='#'>
+
     <script src='js/script.js'></script>
+
 </head>
+
 <body>
+
     <center>
-        <h1> Pannello Amministrativo </h1> 
+
+        <h1> Pannello Amministrativo </h1>
+
         <p> <i> Seleziona <b> Crea Meeting</b> per realizzare un nuovo incontro.</i></p>
+
         <p> <button onclick="window.location.href='creameeting.php'"> Crea meeting </button></p>
+
         <p> <i> Seleziona <b> Modifica Meeting</b> per modificare un incontro.</i></p>
+
         <p> <button onclick="window.location.href='modificameeting.php'"> Modifica meeting </button> </p>
+
         <p> <i> Seleziona <b> Visualizza Meeting</b> per visualizzare i meeting futuri.</i></p>
+
         <p> <button onclick="window.location.href='visualizza.php'"> Visualizza meeting</button> </p>
+
         <p> <i> Seleziona <b> Elimina Meeting</b> per eliminare un incontro.</i></p>
+
         <p> <button onclick="window.location.href='eliminameeting.php'"> Elimina meeting</button> </p>
+
         <p> <button onclick="window.location.href='login.php'"> Esci </button></p>
-        
+
+
+
     </center>
+
 </body>
+
 </html>
--- Lavori in corso/Personale/login.php
+++ Lavori in corso/Personale/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
@@ -8,21 +9,61 @@
     <link rel='stylesheet' type='text/css' media='screen' href='#'>
     <script src='js/script.js'></script>
 </head>
+
 <body>
     <center>
         <!-- Immagine in alto a sx -->
         <a href="login.php"> <img src="#"> </a>
         <!-- Form per l'accesso al pannello amministrativo-->
-        <form action="validateLogin.php" method="post">
-            <h1> Login </h1>
-            <!-- Input username -->
-            <p> Username: <input type="email" name="username" id="i_username"> </p>
-            <!-- Input password -->
-            <p> Password: <input type="password" name="password" id="i_password"> </p>
-            <!-- Bottone per il submit-->
-            <p> <input type="submit" value="Login" name="Accedi"> </p>
-        </form>
-
+        <fieldset style="width:400px">
+            <form action="validator/validatelogin.php" method="post">
+                <h1> Login </h1>
+                <!-- Input username -->
+                <?php
+                /**
+                 * Tabella dei redirect
+                 * redirect type:
+                 * 1 --> campo username vuoto
+                 * 
+                 * 2 --> formato della mail incorretto
+                 * 
+                 * 3 --> campo password vuoto
+                 * 
+                 * 4 --> errore durante la connesione al db
+                 * 
+                 * 5 --> nessun record coincide con quello inserito
+                 * 
+                 * 6 --> il database non contiene record
+                 * 
+                 * 7 --> Session timeout
+                 * 
+                 */
+                if (isset($_GET['error']) && $_GET['error'] == 1) {
+                    echo "<div> <p style='color:red'> Il campo username è vuoto.</p></div>";
+                } else if (isset($_GET['error']) && $_GET['error'] == 2) {
+                    echo "<div> <p style='color:red'> Il formato del campo username è incorretto.</p></div>";
+                } else if (isset($_GET['error']) && $_GET['error'] == 3) {
+                    echo "<div> <p style='color:red'> Il campo password è vuoto </p></div>";
+                } else if (isset($_GET['error']) && $_GET['error'] == 4) {
+                    echo "<div> <p style='color:red'> Errore durante la verifica. </p></div>";
+                } else if (isset($_GET['error']) && $_GET['error'] == 5) {
+                    echo "<div> <p style='color:red'> Login fallito. Registrati se vuoi accedere al portale. </p></div>";
+                } else if (isset($_GET['error']) && $_GET['error'] == 6) {
+                    echo "<div> <p style='color:red'> Login fallito. Registrati se vuoi accedere al portale. </p></div>";
+                } else if (isset($_GET['error']) && $_GET['error'] == 7) {
+                    echo "<div> <p style='color:red'> Timeout session. </p></div>";
+                } else if (isset($_GET['error']) && $_GET['error'] == 8) {
+                    echo "<div> <p style='color:red'> Log registering error. </p></div>";
+                }
+                ?>
+                <p> Username: <input type="text" name="n_username" id="i_username"> </p>
+                <!-- Input password -->
+                <p> Password: <input type="password" name="n_password" id="i_password"> </p>
+                <!-- Bottone per il submit-->
+                <p> <input type="submit" value="Login" name="Accedi"> </p>
+            </form>
+        </fieldset>
     </center>
 </body>
+
 </html>
