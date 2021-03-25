999d58d8c6c9701d1cd08fff24fd4f143ee5fd60
Davide Corbetta <47659574+Corbe-debug@users.noreply.github.com>
2021-03-25 22:55:33 +0100

    Modified php check fields and send email

--- Lavori in corso/Cliente/validator/checkForm.php
+++ Lavori in corso/Cliente/validator/checkForm.php
@@ -5,6 +5,8 @@ session_start();
 if (isset($_GET['submit-meeting'])) {
     //Get values from method GET
     $meetingTime = $_GET["meeting-time"];
+
+
     $name = $_GET["meeting-nameClient"];
     $meetingObject = $_GET["meeting-object"];
     $meetingDescription = $_GET["meeting-description"];
@@ -14,30 +16,37 @@ if (isset($_GET['submit-meeting'])) {
     //Check values
     if (($meetingTime == "") || ($meetingTime == null)) {
         //Return to 'AccessoOspite.php'
-        header("Location: ../AccessoOspite.php?error=Data e ora del meeting non selezionate");
+        header("Location: ../AccessoOspite.php?error=1");
+        exit();
+    } else if ((date('l', strtotime($meetingTime)) == 'Sunday') || (date('l', strtotime($meetingTime)) == 'Saturday')) {
+        //Return to 'AccessoOspite.php'
+        header("Location: ../AccessoOspite.php?error=2");
+        exit();
+    } else if (!checkTime($meetingTime)) {
+        //Return to 'AccessoOspite.php'
+        header("Location: ../AccessoOspite.php?error=11");
         exit();
     } else if (($name == "") || ($name == null)) {
         //Return to 'AccessoOspite.php'
-        header("Location: ../AccessoOspite.php?error=Nominativo non inserito");
+        header("Location: ../AccessoOspite.php?error=3");
         exit();
     } else if (($meetingObject == "") || ($meetingObject == null)) {
         //Return to 'AccessoOspite.php'
-        header("Location: ../AccessoOspite.php?error=Nessun oggetto del meeting inserito");
+        header("Location: ../AccessoOspite.php?error=4");
         exit();
     } else if (($meetingEmployee == "") || ($meetingEmployee == null) || ($meetingEmployee == "Seleziona qualcuno")) {
         //Return to 'AccessoOspite.php'
-        header("Location: ../AccessoOspite.php?error=Nessun impiegato selezionato");
+        header("Location: ../AccessoOspite.php?error=5");
         exit();
     } else if (($meetingEmailClient == "") || ($meetingEmailClient == null)) {
         //Return to 'AccessoOspite.php'
-        header("Location: ../AccessoOspite.php?error=Nessuna email inserita");
+        header("Location: ../AccessoOspite.php?error=6");
         exit();
     } else if (!filter_var($meetingEmailClient, FILTER_VALIDATE_EMAIL)) {
         //Return to 'AccessoOspite.php'
-        header("Location: ../AccessoOspite.php?error=Email non valida");
+        header("Location: ../AccessoOspite.php?error=7");
         exit();
     } else {
-
         //Query
         $sql = "SELECT * FROM Administrator WHERE Id_A = '$meetingEmployee'";
 
@@ -49,18 +58,58 @@ if (isset($_GET['submit-meeting'])) {
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 $emailAdmin = $row['Email'];
+                $nome = $row['Nome'];
+                $cognome = $row['Cognome'];
             }
 
-            $_SESSION['nominativoCliente'] = $name;
+            if (!(($_GET["meeting-description"] == "") || ($_GET["meeting-description"] == null))) {
+                $_SESSION['descrizioneMeeting'] = $_GET["meeting-description"];
+            }
+
+            $_SESSION['oggettoMeeting'] = $meetingObject;
+            $_SESSION['nominativoCliente'] = $cognome . " " . $nome;
             $_SESSION['emailCliente'] = $meetingEmailClient;
-            $_SESSION['to'] = $emailAdmin;
+            $_SESSION['nominativoAdmin'] = $meetingEmployee;
+            $_SESSION['emailAdmin'] = $emailAdmin;
+            $_SESSION['dataora'] = $meetingTime;
 
             header("Location: sendEmail.php");
             exit();
         } else {
             //Return to 'AccessoOspite.php'
-            header("Location: ../AccessoOspite.php?error=Errore invio form, riprova");
+            header("Location: ../AccessoOspite.php?error=8");
             exit();
         }
+    }
+}
+
+//Check if the event's time is correct
+function checkTime($temp)
+{
+    $dt = new DateTime($temp);
+    $date = $dt->format('d-m-Y');
+    $time = $dt->format('h:i A');
+
+    //Check if the hour are 12
+    $time1 = $dt->format('h:i');
+    $temp = explode(":", $time1);
+    $t = intval($temp[0]);
+
+    if ((strpos($time, "PM")) && ($t != 12)) {
+        $time = $dt->format('h:i');
+        $temp = explode(":", $time);
+        $t = intval($temp[0]);
+        $t += 12;
+        $time = $t . ":" . $temp[1];
+    }
+
+    $orarioapertura = new DateTime("08:00");
+    $orariochiusura = new DateTime("17:30");
+    $orario = new DateTime($time);
+
+    if ($orario >= $orarioapertura && $orario <= $orariochiusura) {
+        return true;
+    } else {
+        return false;
     }
 }
--- Lavori in corso/Cliente/validator/sendEmail.php
+++ Lavori in corso/Cliente/validator/sendEmail.php
@@ -1,16 +1,54 @@
 <?php
 session_start();
 
-if ((isset($_SESSION['to'])) && (isset($_SESSION['nominativoCliente'])) && (isset($_SESSION['emailCliente']))) {
+//Check sessions
+if ((isset($_SESSION['oggettoMeeting'])) && (isset($_SESSION['nominativoCliente'])) && (isset($_SESSION['emailCliente'])) && (isset($_SESSION['nominativoAdmin'])) && (isset($_SESSION['emailAdmin'])) && (isset($_SESSION['dataora']))) {
     //Require class phpmailer
     require_once('phpmailer/PHPMailerAutoload.php');
 
-    //Get email
-    $to = $_SESSION['to'];
+    //Get element of event
+    $oggettoMeeting = $_SESSION['oggettoMeeting'];
     $nominativoCliente = $_SESSION['nominativoCliente'];
     $emailCliente = $_SESSION['emailCliente'];
+    $nominativoAdmin = $_SESSION['nominativoAdmin'];
+    $emailAdmin = $_SESSION['emailAdmin'];
+    $dataora = $_SESSION['dataora'];
 
-    //Create e-mail
+    //Setting event
+    //Check if meeting description isset
+    $checkDescription = false;
+    if (!isset($_SESSION['descrizioneMeeting'])) {
+        $ical_content = "BEGIN:VCALENDAR
+        VERSION:2.0
+        PRODID://Drupal iCal API//EN
+        BEGIN:VEVENT
+        UID:http://www.icalmaker.com/event/d8fefcc9-a576-4432-8b20-40e90889affd
+        DTSTAMP:$dataora
+        DTSTART:$dataora
+        DTEND:$dataora
+        SUMMARY:$oggettoMeeting
+        LOCATION:Sede L2GM
+        END:VEVENT
+        END:VCALENDAR";
+    } else {
+        $descrizione = $_SESSION['descrizioneMeeting'];
+        $checkDescription = true;
+        $ical_content = "BEGIN:VCALENDAR
+        VERSION:2.0
+        PRODID://Drupal iCal API//EN
+        BEGIN:VEVENT
+        UID:http://www.icalmaker.com/event/d8fefcc9-a576-4432-8b20-40e90889affd
+        DTSTAMP:$dataora
+        DTSTART:$dataora
+        DTEND:$dataora
+        SUMMARY:$oggettoMeeting
+        LOCATION:Sede L2GM
+        DESCRIPTION:$descrizione
+        END:VEVENT
+        END:VCALENDAR";
+    }
+
+    //Create e-mail's structure
     $mail = new PHPMailer();
     $mail->isSMTP();
     $mail->SMTPAuth = true;
@@ -22,22 +60,30 @@ if ((isset($_SESSION['to'])) && (isset($_SESSION['nominativoCliente'])) && (isse
     $mail->Password = 'test12342021';
     $mail->setFrom('testdavide2021@gmail.com');
     $mail->Subject = 'Richiesta appuntamento';
-    $mail->Body = "<h1>Richiesta appuntamento da $emailCliente</h1>
-                <p>$nominativoCliente richiede un appuntamento con te</p>";
-    $mail->addAddress($to);
+    $mail->Body = "<h1>Richiesta appuntamento da $emailCliente</h1><p>$nominativoCliente richiede un appuntamento</p><p>Oggetto: $oggettoMeeting</p>";
 
-    //Send
+    $mail->addAddress($emailAdmin);
+
+    //Check sending email
     if ($mail->send()) {
         //Return to 'AccessoOspite.php'
-        header("Location: ../AccessoOspite.php?ok=Email inviata con successo");
+        header("Location: ../AccessoOspite.php?ok=1");
+        deleteSession();
         exit();
     } else {
         //Return to 'AccessoOspite.php'
-        header("Location: ../AccessoOspite.php?error=Errore nell'invio dell'email di richiesta, riprova");
+        header("Location: ../AccessoOspite.php?error=9");
+        deleteSession();
         exit();
     }
 } else {
     //Return to 'AccessoOspite.php'
-    header("Location: ../AccessoOspite.php");
+    header("Location: ../AccessoOspite.php?error=10");
     exit();
+}
+
+//Delete all sessions
+function deleteSession()
+{
+    session_destroy();
 }
