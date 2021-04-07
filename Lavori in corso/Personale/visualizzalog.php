997086fd8dfed87d6c9baa3ee8f653e4dc066e5f
Cacciaa <matcacciarino@gmail.com>
2021-04-07 16:58:07 +0200

    Added responsive CSS for "visualizzalog.php"

--- Lavori in corso/Personale/visualizzalog.php
+++ Lavori in corso/Personale/visualizzalog.php
@@ -56,28 +56,91 @@ function redirect($errortype)
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
+    <link rel="icon" type=“image/x-icon” href="faviconFolder/favicon001.ico" />
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="css/styleVisualizzaLog.css" />
+
+    <style>
+        body,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+            font-family: "Raleway", sans-serif
+        }
+
+        .w3-bar .w3-button {
+
+            display: block;
+            padding: 0px;
+
+            border-bottom: 2px solid transparent;
+
+            transition: border-bottom-color 0.5s ease-out;
+            -webkit-transition: border-bottom-color 0.5s ease-out;
+        }
+
+
+        .w3-button:hover {
+            color: blue;
+            border-bottom-color: blue;
+        }
+    </style>
+
 </head>
 
-<body>
+<body style="background-color: #F5F5F5;">
+
+
+    <!-- Navbar -->
+    <div class="w3-top">
+        <div class="w3-bar w3-white w3-padding w3-card" style="letter-spacing:4px;">
+            <a href="visualizza.php" class="w3-bar-item w3-button w3-wide"><img src="immaginiPersonale/logo.png"></a>
+            <!-- Parte destra navbar(opzionale) -->
+            <div class="w3-right w3-hide-small">
+            </div>
+
+
+        </div>
+    </div>
+
     <center>
-        <h1> Visualizza Log</h1>
 
-        <table>
-            <!-- Stampa della query i log d'accesso-->
-            <?php
+    <div id="container"> 
 
-            echo $table;
-            ?>
-        </table>
+            <h2>Visualizza Log</h2>
+            <br>
+            <hr class="linea">
 
+            <br>
+            <br>
 
-    </center>
+        <div id="container2">    
 
-    <footer>
+            <table>
+                <!-- Stampa della query i log d'accesso-->
+                <?php
 
-        <button id="i_bback" name="n_bback" onclick="window.location.href='visualizza.php'"> Indietro </button>
+                echo $table;
+                ?>
+            </table>
 
-    </footer>
+        </div>
+
+    </div>
+    
+    <br>
+
+    <button id="i_bback" name="n_bback" onclick="window.location.href='visualizza.php'"> Indietro </button>
+
+    <br>
+    <br>
+
+    </center> 
+
 </body>
 
 </html>
