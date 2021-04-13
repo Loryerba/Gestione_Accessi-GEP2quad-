bb5d003e2d3c0bcdccc74fedf440e537f4ee032b
Cacciaa <matcacciarino@gmail.com>
2021-04-13 18:22:13 +0200

    Added Responsive CSS for "modificaRecord.php"

--- Lavori in corso/Personale/modificaRecord.php
+++ Lavori in corso/Personale/modificaRecord.php
@@ -76,14 +76,69 @@ function redirect($errortype)
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='js/script.js'></script>
+    <link rel="icon" type=“image/x-icon” href="faviconFolder/favicon001.ico" />
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="css/styleModificaRecord.css" />
+
+    <style>
+        body,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+            font-family: "Raleway", sans-serif
+        }
+
+        .w3-bar .w3-button {
+
+            display: block;
+            padding: 0px;
+
+            border-bottom: 2px solid transparent;
+
+            transition: border-bottom-color 0.5s ease-out;
+            -webkit-transition: border-bottom-color 0.5s ease-out;
+        }
+
+
+        .w3-button:hover {
+            color: blue;
+            border-bottom-color: blue;
+        }
+    </style>
+
 </head>
 
 
-<body>
+<body style="background-color: #59c0f349;">
+
+    <!-- Navbar -->
+    <div class="w3-top">
+        <div class="w3-bar w3-white w3-padding w3-card" style="letter-spacing:4px;">
+            <a href="adminPnl.php" class="w3-bar-item w3-button w3-wide"><img src="immaginiPersonale/logo.png"></a>
+            <!-- Parte destra navbar(opzionale) -->
+            <div class="w3-right w3-hide-small">
+            </div>
+
+
+        </div>
+    </div>
+
     <center>
-        <h2> Modifica del meeting selezionato</h2>
+        
         <!--Ricevere via get il record selezionato e prevedo la modifica dei campi-->
         <form action='validator/validateModificaRecord.php' method='post'>
+        <div id="container">
+        <h2> Modifica del meeting selezionato</h2>
+        <br>
+        <hr class="linea">
+
+        <br>        
+        <br>
+
             <table>
                 <tr>
                     <th> Id Meeting</th>
@@ -107,9 +162,10 @@ function redirect($errortype)
                 ?>
             </table>
 
-            <p><input type='submit' value='Conferma modifiche'> </p>
+            <p><input type='submit' id = "n_submit" name="n_submit" value='Conferma modifiche'> </p>
+            </div>
         </form>
-        <button onclick="window.location.href='modificameeting.php'"> Indietro </button>
+        <button id="i_bback" name="n_bback" onclick="window.location.href='modificameeting.php'"> Indietro </button>
     </center>
 </body>
 
